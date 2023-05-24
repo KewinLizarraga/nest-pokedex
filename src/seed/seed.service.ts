ce45e93ce59e0352445f27c87704b51ebfba8407
kLizarraga <kewin392@gmail.com>
2023-05-24 03:44:07 -0500

    Register many pokemons in a single insert

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
+    await this.pokemonModel.deleteMany({}); // delete * from pokemons
+
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
+    //* Si son muchos registros, ej 500, puede ocurrir un error y el servidor se cae
+    // data.results.forEach(async ({ name, url }) => {
+    //   const segments = url.split('/');
+    //   const no = +segments[segments.length - 2];
+    //   const pokemon = await this.pokemonModel.create({ name, no });
+    // });
+
+    //* Para evitar eso, implementamos cualquiera de las siguientes formas
+    //* Forma 1: Usando promesas, el Promise.all permite pasar de uno en uno asegurando que todos se registren
+    // const insertPromisesArray = [];
+    // data.results.forEach(({ name, url }) => {
+    //   const segments = url.split('/');
+    //   const no = +segments[segments.length - 2];
+    //   insertPromisesArray.push(this.pokemonModel.create({ name, no }));
+    // });
+    // await Promise.all(insertPromisesArray);
+    //* Forma 2: Usando insertMany, realiza una inserción con muchos registros
+    const pokemonToInsert: { name: string; no: number }[] = [];
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({ name, no });
+      pokemonToInsert.push({ name, no });
     });
+    await this.pokemonModel.insertMany(pokemonToInsert);
 
-    return data.results;
+    return 'Seed excuted';
   }
 }
